6d6f3ebb944ff7e6ca35cc6752dfbc923e2423ca
Collin Argo <collinargo@gmail.com>
2018-09-24 18:55:36 -0400

    add spiral function

--- golden_turtle.py
+++ golden_turtle.py
@@ -10,25 +10,39 @@ turtle_on_screen = True
 screen = Screen()
 screen.setup(400, 400)
 
-def most_of_a_rectangle(short_side, long_side):
-    turtle.forward(short_side)
-    turtle.right(90)
-    turtle.forward(long_side)
-    turtle.right(90)
-    turtle.forward(short_side)
-    turtle.right(90)
+def golden_rectangle(short_side, long_side):
+    turtle_on_screen = True
+    while turtle_on_screen:
+        turtle.forward(short_side)
+        turtle.right(90)
+        turtle.forward(long_side)
+        turtle.right(90)
+        turtle.forward(short_side)
+        turtle.right(90)
+        short_side = long_side
+        long_side = short_side * GOLDEN_RATIO
+        if turtle.ycor() > 400 or turtle.xcor() > 400:
+            turtle_on_screen = False
+
+def golden_spiral():
+    radius = 1
+    while turtle_on_screen:
+        turtle.circle(radius, extent=90)
+        radius *= GOLDEN_RATIO
+
+
+def turtle_home():
+    turtle.penup()
+    turtle.goto(0,0)
+    turtle.setheading(270)
+    turtle.pendown()
+    turtle_on_screen = True
+
 
 short_side = 1
 long_side = GOLDEN_RATIO
-
-while turtle_on_screen:
-    most_of_a_rectangle(short_side, long_side)
-    short_side = long_side
-    long_side = short_side * GOLDEN_RATIO
-    if turtle.ycor() > 400 or turtle.xcor() > 400:
-        turtle_on_screen = False
-
-turtle.penup()
-turtle.goto(0,0)
-turtle_on_screen = True
+golden_rectangle(short_side, long_side)
+turtle_home()
+golden_spiral()
+turtle_home()
 input()
