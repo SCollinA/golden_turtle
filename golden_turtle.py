21f2626c1200d5b71899e89d93e920bfea21c535
Collin Argo <collinargo@gmail.com>
2018-09-24 19:11:28 -0400

    draw 4 rectangles and spirals

--- golden_turtle.py
+++ golden_turtle.py
@@ -5,7 +5,7 @@ from turtle import *
 GOLDEN_RATIO = 1.618
 
 turtle = Turtle()
-turtle.speed(0)
+turtle.speed(6)
 turtle_on_screen = True
 screen = Screen()
 screen.setup(400, 400)
@@ -25,16 +25,19 @@ def golden_rectangle(short_side, long_side):
             turtle_on_screen = False
 
 def golden_spiral():
+    turtle_on_screen = True
     radius = 1
     while turtle_on_screen:
         turtle.circle(radius, extent=90)
         radius *= GOLDEN_RATIO
+        if turtle.ycor() > 400 or turtle.xcor() > 400:
+            turtle_on_screen = False
 
 
 def turtle_home():
     turtle.penup()
     turtle.goto(0,0)
-    turtle.setheading(270)
+    turtle.setheading(0)
     turtle.pendown()
     turtle_on_screen = True
 
@@ -43,6 +46,26 @@ short_side = 1
 long_side = GOLDEN_RATIO
 golden_rectangle(short_side, long_side)
 turtle_home()
+turtle.right(90)
 golden_spiral()
 turtle_home()
+turtle.right(180)
+golden_rectangle(short_side, long_side)
+turtle_home()
+turtle.right(270)
+golden_spiral()
+turtle_home()
+turtle.right(90)
+golden_rectangle(short_side, long_side)
+turtle_home()
+turtle.right(180)
+golden_spiral()
+turtle_home()
+turtle.right(270)
+golden_rectangle(short_side, long_side)
+turtle_home()
+turtle.right(360)
+golden_spiral()
+
+
 input()
