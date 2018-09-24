f2872929f05f685da1a7a607da1c2095d971a8b5
Collin Argo <collinargo@gmail.com>
2018-09-24 18:21:30 -0400

    lift pen when moving turtle home

--- golden_turtle.py
+++ golden_turtle.py
@@ -5,8 +5,10 @@ from turtle import *
 GOLDEN_RATIO = 1.618
 
 turtle = Turtle()
+turtle.speed(0)
+turtle_on_screen = True
 screen = Screen()
-screen.setup(1000, 1000)
+screen.setup(400, 400)
 
 def most_of_a_rectangle(short_side, long_side):
     turtle.forward(short_side)
@@ -19,10 +21,14 @@ def most_of_a_rectangle(short_side, long_side):
 short_side = 1
 long_side = GOLDEN_RATIO
 
-while True:
+while turtle_on_screen:
     most_of_a_rectangle(short_side, long_side)
     short_side = long_side
     long_side = short_side * GOLDEN_RATIO
+    if turtle.ycor() > 400 or turtle.xcor() > 400:
+        turtle_on_screen = False
 
-
-
+turtle.penup()
+turtle.goto(0,0)
+turtle_on_screen = True
+input()
